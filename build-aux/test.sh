a86bf7a6b3916eee7354850147f11bcbd00f32ab
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2024-05-05 20:27:56 +0200

    build: add test runner script

---
+++ build-aux/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+if [[ "${VARIANT:-}" = "memcheck" ]]; then
+	export VALGRIND=valgrind
+	export TESTS_ENVIRONMENT="
+		${VALGRIND} \
+			--tool=memcheck \
+			--leak-check=full \
+			--leak-resolution=high \
+			--num-callers=20 \
+			--error-exitcode=1 \
+			--show-possibly-lost=no"
+fi
+
+if [[ "${1}" == *.sh ]]; then
+	exec "${@}"
+else
+	exec ${TESTS_ENVIRONMENT:-} "${@}"
+fi
